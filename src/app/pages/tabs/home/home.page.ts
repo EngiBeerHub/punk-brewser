0eebc68a4773092143a4476ff3a0181281603f69
EngiBeerHub <engibeer0422@gmail.com>
2023-12-05 00:16:56 +0900

    [migrate]Added detailed data

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,101 +1,109 @@
 import {Component, OnInit} from '@angular/core';
 import {
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonContent,
-  IonHeader,
-  IonIcon,
-  IonImg,
-  IonRefresher,
-  IonRefresherContent,
-  IonSkeletonText,
-  IonText,
-  IonThumbnail,
-  IonTitle,
-  IonToolbar
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonImg,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonRefresher,
+    IonRefresherContent,
+    IonSkeletonText,
+    IonText,
+    IonThumbnail,
+    IonTitle,
+    IonToolbar
 } from "@ionic/angular/standalone";
 import {Beer} from "../../../models/beer";
 import {BeerService} from "../../../services/beer.service";
-import {NgClass, NgIf} from "@angular/common";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {RefresherCustomEvent} from "@ionic/angular";
 
 @Component({
-  // selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonCard,
-    IonCardHeader,
-    IonCardTitle,
-    IonCardSubtitle,
-    IonCardContent,
-    IonImg,
-    IonText,
-    NgIf,
-    IonIcon,
-    IonSkeletonText,
-    IonThumbnail,
-    NgClass,
-    IonRefresher,
-    IonRefresherContent
-  ],
+    // selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+    standalone: true,
+    imports: [
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        IonCard,
+        IonCardHeader,
+        IonCardTitle,
+        IonCardSubtitle,
+        IonCardContent,
+        IonImg,
+        IonText,
+        NgIf,
+        IonIcon,
+        IonSkeletonText,
+        IonThumbnail,
+        NgClass,
+        IonRefresher,
+        IonRefresherContent,
+        IonList,
+        IonItem,
+        IonLabel,
+        NgForOf
+    ],
 })
 export class HomePage implements OnInit {
-  beer?: Beer;
-  isLoadedBeer = false;
-  imageUrl?: string;
-  private readonly altImageUrl = 'https://images.punkapi.com/v2/keg.png';
+    beer?: Beer;
+    beerDetails = new Map<string, any>;
+    isLoadedBeer = false;
+    imageUrl?: string;
+    private readonly altImageUrl = 'https://images.punkapi.com/v2/keg.png';
 
-  constructor(
-    private beerService: BeerService
-  ) {
-  }
+    constructor(
+        private beerService: BeerService
+    ) {
+    }
 
-  ngOnInit(): void {
-    this.fetchRandomBeer();
-  }
+    ngOnInit(): void {
+        this.fetchRandomBeer();
+    }
 
-  /**
-   * Handle pull to refresh
-   * @param event
-   */
-  handleRefresh(event: RefresherCustomEvent) {
-    this.isLoadedBeer = false;
-    this.fetchRandomBeer(event);
-  }
+    /**
+     * Handle pull to refresh
+     * @param event
+     */
+    handleRefresh(event: RefresherCustomEvent) {
+        this.isLoadedBeer = false;
+        this.fetchRandomBeer(event);
+    }
 
-  /**
-   * Fetch a random beer from API
-   * @param event
-   * @private
-   */
-  private fetchRandomBeer(event?: RefresherCustomEvent) {
-    this.beerService.getRandomBeer().subscribe({
-      next: (fetchedBeer) => {
-        this.beer = fetchedBeer;
-        // set alternative keg image when image url is null.
-        this.beer.image_url
-          ? (this.imageUrl = this.beer.image_url)
-          : (this.imageUrl = this.altImageUrl);
-        // show card
-        this.isLoadedBeer = true;
-        // when pull to refresh, complete is necessary
-        event?.target.complete();
-      },
-      error: () => {
-        // TODO: not implemented yet.
-        // when pull to refresh, complete is necessary
-        event?.target.complete();
-      },
-      complete: () => console.log('getRandomBeer completed.'),
-    });
-  }
+    /**
+     * Fetch a random beer from API
+     * @param event
+     * @private
+     */
+    private fetchRandomBeer(event?: RefresherCustomEvent) {
+        this.beerService.getRandomBeer().subscribe({
+            next: (fetchedBeer) => {
+                this.beer = fetchedBeer;
+                // set alternative keg image when image url is null.
+                this.beer.image_url
+                    ? (this.imageUrl = this.beer.image_url)
+                    : (this.imageUrl = this.altImageUrl);
+                // show card
+                this.isLoadedBeer = true;
+                // when pull to refresh, complete is necessary
+                event?.target.complete();
+            },
+            error: () => {
+                // TODO: not implemented yet.
+                // when pull to refresh, complete is necessary
+                event?.target.complete();
+            },
+            complete: () => console.log('getRandomBeer completed.'),
+        });
+    }
 }
--- src/app/pages/tabs/tab3/tab3.page.ts
+++ src/app/pages/tabs/tab3/tab3.page.ts
@@ -1,18 +1,14 @@
-import { Component } from '@angular/core';
-import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-} from '@ionic/angular/standalone';
+import {Component} from '@angular/core';
+import {IonContent, IonHeader, IonIcon, IonTitle, IonToolbar,} from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+    selector: 'app-tab3',
+    templateUrl: 'tab3.page.html',
+    styleUrls: ['tab3.page.scss'],
+    standalone: true,
+    imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon],
 })
 export class Tab3Page {
-  constructor() {}
+    constructor() {
+    }
 }
